7c14fe9eede7bd23d4fee88bcc89d799434d3f1a
David Jongepier <david.jongepier@gmail.com>
2015-03-09 15:20:19 +0100

    Initial Commit

---
+++ WeatherForecast/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WeatherForecast
+//
+//  Created by David Jongepier on 09.03.15.
+//  Copyright (c) 2015 STRV. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
